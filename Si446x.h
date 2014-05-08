e84b58202c3ead471a6ce8d284a371555f883f2a
Ahmet Ipkin <ahmetipkin@gmail.com>
2014-05-09 01:15:33 +0300

    Initial commit

---
+++ Si446x.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef Si446x_H_
+#define Si446x_H_
+#include "Arduino.h"
+//add your includes for the project Si446x here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project Si446x here
+
+
+
+
+//Do not add code below this line
+#endif /* Si446x_H_ */
